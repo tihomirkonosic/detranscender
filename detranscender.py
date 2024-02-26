c2e2ef0721ef4b6f91c803540d1ecd3f67526efe
Tihomir Konosic <tihomir.konosic@dalafiti.hr>
2024-02-26 18:23:36 +0100

    added length checking

--- detranscender.py
+++ detranscender.py
@@ -1,6 +1,9 @@
-import dgl.data.utils as dglutils
 import os
 import time
+
+import dgl
+import dgl.data.utils as dgl_utils
+import torch
 
 
 def export_graph_to_csv(g, path):
@@ -9,24 +12,48 @@ def export_graph_to_csv(g, path):
             f.write(f"{e[0].item()},{e[1].item()}\n")
 
 
-def remove_transitive_edges(graph):
-    candidates = [-1] * graph.number_of_nodes()
+def is_comparable(g, edge_a, edge_b, edge_c):
+    eps = 0.2
+    overlap_lengths = g.edata["overlap_length"]
+    a = overlap_lengths[edge_a].item() + overlap_lengths[edge_b].item()
+    b = overlap_lengths[edge_c].item()
+    return (b * (1 - eps) <= a <= b * (1 + eps)) or (a * (1 - eps) <= b <= a * (1 + eps))
+
+
+def remove_transitive_edges(g):
+    candidates = [-1] * g.number_of_nodes()
     marked_edges = []
-    for n in graph.nodes():
-        out_edges = graph.out_edges(n, form="all")
+    for n in g.nodes():
+        out_edges = g.out_edges(n, form="all")
         for ei in range(out_edges[0].shape[0]):
             candidates[out_edges[1][ei].item()] = out_edges[2][ei].item()
 
         for ei in range(out_edges[0].shape[0]):
-            out_edges2 = graph.out_edges(out_edges[1][ei].item(), form="all")
+            out_edges2 = g.out_edges(out_edges[1][ei].item(), form="all")
             for ei2 in range(out_edges2[0].shape[0]):
-                if candidates[out_edges2[1][ei2].item()] != -1:
+                if (candidates[out_edges2[1][ei2].item()] != -1 and
+                        is_comparable(g,
+                                      out_edges[2][ei].item(),
+                                      out_edges2[2][ei2].item(),
+                                      candidates[out_edges2[1][ei2].item()])):
                     marked_edges.append(candidates[out_edges2[1][ei2].item()])
 
-        candidates = [-1] * graph.number_of_nodes()
+        candidates = [-1] * g.number_of_nodes()
 
-    graph.remove_edges(marked_edges)
-    print(f"Removed: {len(marked_edges)} edges")
+    edges_before = g.number_of_edges()
+    g.remove_edges(marked_edges)
+    print(f"Removed transitive edges; after: {g.number_of_edges()}; before: {edges_before}")
+
+
+def load_graph(path):
+    glist, label_dict = dgl_utils.load_graphs(path)
+    return glist[0]
+
+
+def generate_test_graph():
+    g = dgl.graph((torch.tensor([0, 0, 0, 0, 1, 2, 3]), torch.tensor([1, 2, 3, 4, 2, 3, 4])))
+    g.edata["overlap_length"] = torch.tensor([100, 200, 700, 800, 100, 100, 100])
+    return g
 
 
 if __name__ == "__main__":
@@ -45,19 +72,18 @@ if __name__ == "__main__":
     if not isExist:
         os.makedirs(out_folder)
 
-    glist, label_dict = dglutils.load_graphs(input_path)
-    g = glist[0]
+    graph = load_graph(input_path)
+    # graph = generate_test_graph()
 
-    # Test
-    # g = dgl.graph((torch.tensor([1, 1, 1, 1, 2, 3, 4]), torch.tensor([2, 3, 4, 5, 3, 4, 5])))
+    export_graph_to_csv(graph, before_csv_path)
 
-    export_graph_to_csv(g, before_csv_path)
+    print("Graph loaded")
 
     start_time = time.time()
-    remove_transitive_edges(g)
+    remove_transitive_edges(graph)
     end_time = time.time()
 
-    dglutils.save_graphs(output_path, glist)
-    export_graph_to_csv(g, after_csv_path)
+    dgl_utils.save_graphs(output_path, [graph])
+    export_graph_to_csv(graph, after_csv_path)
 
     print(f"Finished: time={end_time-start_time} seconds")
