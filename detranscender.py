ca8611420f2295971af2fdea63b8b9565b305ea7
Tihomir Konosic <tihomir.konosic@dalafiti.hr>
2024-02-26 15:44:33 +0100

    working

--- detranscender.py
+++ detranscender.py
@@ -1,33 +1,63 @@
 import dgl.data.utils as dglutils
-import dgl
-import torch
-
-
-def load_graph():
-    glist, label_dict = dglutils.load_graphs("test/chr19_hifi30x.dgl")
-    return glist, label_dict
+import os
+import time
 
 
 def export_graph_to_csv(g, path):
     with open(path, "w") as f:
-        for src, dst in zip(g.edges()[0].tolist(), g.edges()[1].tolist()):
-            f.write(f"{src},{dst}\n")
+        for e in zip(*g.edges()):
+            f.write(f"{e[0].item()},{e[1].item()}\n")
 
 
 def remove_transitive_edges(graph):
-#    for n in graph.nodes():
-#        for e in graph.out_edges():
-    return 0
+    candidates = [-1] * graph.number_of_nodes()
+    marked_edges = []
+    for n in graph.nodes():
+        out_edges = graph.out_edges(n, form="all")
+        for ei in range(out_edges[0].shape[0]):
+            candidates[out_edges[1][ei].item()] = out_edges[2][ei].item()
+
+        for ei in range(out_edges[0].shape[0]):
+            out_edges2 = graph.out_edges(out_edges[1][ei].item(), form="all")
+            for ei2 in range(out_edges2[0].shape[0]):
+                if candidates[out_edges2[1][ei2].item()] != -1:
+                    marked_edges.append(candidates[out_edges2[1][ei2].item()])
+
+        candidates = [-1] * graph.number_of_nodes()
+
+    graph.remove_edges(marked_edges)
+    print(f"Removed: {len(marked_edges)} edges")
 
 
 if __name__ == "__main__":
+
+    input_path = "test/chr19_hifi30x.dgl"
+
+    out_folder = "out"
+    output_path = "out/chr19_hifi30x_optimised.dgl"
+
+    before_csv_path = "out/before.csv"
+    after_csv_path = "out/after.csv"
+
     print("Start:")
 
-    #glist, label_dict = load_graph()
-    #g = glist[0]
+    isExist = os.path.exists(out_folder)
+    if not isExist:
+        os.makedirs(out_folder)
 
-    g = dgl.graph((torch.tensor([1, 1, 1, 1, 2, 3, 4]), torch.tensor([2, 3, 4, 5, 3, 4, 5])))
+    glist, label_dict = dglutils.load_graphs(input_path)
+    g = glist[0]
 
-    export_graph_to_csv(g, "out/before.csv")
+    # Test
+    # g = dgl.graph((torch.tensor([1, 1, 1, 1, 2, 3, 4]), torch.tensor([2, 3, 4, 5, 3, 4, 5])))
 
-    print("Loaded")
+    export_graph_to_csv(g, before_csv_path)
+
+    start_time = time.time()
+    remove_transitive_edges(g)
+    end_time = time.time()
+
+    dglutils.save_graphs(output_path, glist)
+    export_graph_to_csv(g, after_csv_path)
+
+    print(f"Finished: time={end_time-start_time} seconds")
